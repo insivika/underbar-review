eeeac998374098dbac6229e075c3702b90a2e07f
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-06-23 17:27:18 -0700

    pluck passing

--- spec/part1.js
+++ spec/part1.js
@@ -331,7 +331,7 @@
       it('should handle iterators that work with a sorted array', function() {
         var iterator = function(value) { return value === 1; };
         var numbers = [1, 2, 2, 3, 4, 4];
-        console.log(iterator)
+        console.log(iterator);
         expect(_.uniq(numbers, iterator)).to.eql([1, 2]);
       });
 
@@ -376,7 +376,7 @@
       });
 
       it('should apply a function to every value in an array', function() {
-        var multiplyByTwo = FILL_ME_IN;
+        var multiplyByTwo = function(value) {return value * 2}
 
         expect(_.map([1, 2, 3], multiplyByTwo)).to.eql([2, 4, 6]);
       });
@@ -399,7 +399,7 @@
           { name: 'curly', age: 50 }
         ];
 
-        expect(_.pluck(people, 'name')).to.FILL_ME_IN(['moe', 'curly']);
+        expect(_.pluck(people, 'name')).to.eql(['moe', 'curly']);
       });
 
       it('should not modify the original array', function() {
@@ -410,7 +410,7 @@
 
         _.pluck(people, 'name');
 
-        expect(people).to.FILL_ME_IN([{ name: 'moe', age: 30 }, { name: 'curly', age: 50 }]);
+        expect(people).to.not.equal([{ name: 'moe', age: 30 }, { name: 'curly', age: 50 }]);
       });
     });
 
--- src/underbar.js
+++ src/underbar.js
@@ -146,7 +146,15 @@
   _.map = function(collection, iterator) {
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
-    // the members, it also maintains an array of results.
+    // the members, it also maintains an array of results.  
+var arr = [];
+
+_.each(collection, function(ele){
+       arr.push(iterator(ele));
+})
+
+return arr;
+
   };
 
   /*
@@ -188,6 +196,7 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    
   };
 
   // Determine if the array or object contains a given value (using `===`).
